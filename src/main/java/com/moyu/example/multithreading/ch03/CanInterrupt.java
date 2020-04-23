4e4299281416da05221a92cc71acb0d13cdccb8d
jianfeng.li <jianfeng.li@100credit.com>
2020-04-23 16:01:36 +0800

    java线程中断失效

--- src/main/java/com/moyu/example/multithreading/ch03/CanInterrupt.java
+++ src/main/java/com/moyu/example/multithreading/ch03/CanInterrupt.java
@@ -14,16 +14,17 @@ public class CanInterrupt {
     public static Runnable getRunnable() {
         return () -> {
             int num = 0;
-            while (num < 100000) {
+            // ①    加入线程中断状态也无效, 已经被sleep清空了状态
+            while (num < 100000 /* && !Thread.currentThread().isInterrupted()*/) {
                 System.out.println("当前值为: " + num);
                 try {
                     Thread.sleep(300);
-//                    System.out.println("当前值为: " + num);   ①   不会输出
+//                    System.out.println("当前值为: " + num);   ②   不会输出
                 } catch (InterruptedException e) {
                     e.printStackTrace();
 
-//                    int i = 1 / 0;                           ②   抛出异常后终止
-//                    return ;                                 ③   退出
+//                    int i = 1 / 0;                           ③   抛出异常后终止
+//                    return ;                                 ④   退出
                 }
 
                 num ++;
