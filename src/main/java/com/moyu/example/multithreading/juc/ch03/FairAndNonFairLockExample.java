90e84c9c20d09aa0104c4d99a758d693666d223a
jianfeng.li <jianfeng.li@100credit.com>
2020-07-14 15:50:30 +0800

    公平锁&非公平锁实例

--- src/main/java/com/moyu/example/multithreading/juc/ch03/FairAndNonFairLockExample.java
+++ src/main/java/com/moyu/example/multithreading/juc/ch03/FairAndNonFairLockExample.java
@@ -13,15 +13,50 @@ public class FairAndNonFairLockExample {
     // 非公平锁
     private static Lock lock = new ReentrantLock(false);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         for (int i = 0; i < 10; i++) {
             new Thread(task(), "Thread-" + i).start();
+
+            // 按顺序启动线程, 避免在启动的过程中造成优先问题
+            // 现在线程的启动执行顺序一定是Thread-0到Thread-9
+            Thread.sleep(100);
         }
     }
 
 
     private static Runnable task() {
         return () -> {
+
+
+            /****
+             *
+             *      如果是公平锁:
+             *          如果是公平锁的话, 首先Thread-0获取到锁, 然后执行, 但是期间Thread-1到Thread-9的线程
+             *          都被加入到等待队列中了, 所以, Thread-0想在一次打水的话会加入到队列的队尾中。
+             *
+             *          此时, 按照我们的构想, 队列应该为
+             *             Thread-1 -> Thread-2 -> Thread-3 -> Thread-4 -> Thread-5 ->
+             *             Thread-6 -> Thread-7 -> Thread-8 -> Thread-9 -> Thread-0
+             *
+             *          所以, 我们打印输出的结果顺序一定是:
+             *              Thread-0 -> Thread-1 -> Thread-2 -> Thread-3 -> Thread-4
+             *              Thread-5 -> Thread-6 -> Thread-7 -> Thread-8 -> Thread-9
+             *              ...
+             *              Thread-0 -> Thread-1 -> Thread-2 -> Thread-3 -> Thread-4
+             *              Thread-5 -> Thread-6 -> Thread-7 -> Thread-8 -> Thread-9
+             *
+             *
+             *
+             *      如果是非公平锁:
+             *          如果是非公平锁的话, 第一个线程Thread-0仍然是先获取到锁, 但是在执行过程中
+             *          Thread-1到Thread-9都进入等待队列中排队去了....
+             *
+             *          此时, 我们第一次打水完成后, 线程Thread-0释放锁, 但是请注意, 当前线程Thread-0
+             *          还没有进入阻塞状态, 而我们要唤醒等待队列中的线程是需要时间的, 所以线程Thread-0
+             *          可能会再一次获取到锁, 从而在打水一次。
+             *
+             */
+
             try {
                 lock.lock();
                 System.out.println(Thread.currentThread().getName() + "获取到锁...");
