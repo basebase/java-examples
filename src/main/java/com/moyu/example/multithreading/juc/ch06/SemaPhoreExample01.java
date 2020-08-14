291d6e380f9f0e9510645949760aa63aaf7e471c
jianfeng.li <jianfeng.li@100credit.com>
2020-08-14 16:24:40 +0800

    SemaPhore例子

---
+++ src/main/java/com/moyu/example/multithreading/juc/ch06/SemaPhoreExample01.java
@@ -0,0 +1,43 @@
+package com.moyu.example.multithreading.juc.ch06;
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/***
+ *      描述:     SemaPhore使用例子, 控制线程访问量
+ */
+
+public class SemaPhoreExample01 {
+
+    public static void main(String[] args) {
+
+        // 创建信号量
+        Semaphore semaphore = new Semaphore(3);
+
+        ExecutorService executorService =
+                Executors.newFixedThreadPool(50);
+
+        for (int i = 0; i < 50; i++) {
+            final String id = "User-" + (i + 1);
+            executorService.execute(() -> {
+                System.out.println(Thread.currentThread().getName() + " : " + id + " 申请产假...");
+                try {
+                    semaphore.acquire();
+                    System.out.println(Thread.currentThread().getName() + " : " + id + " 申请产假成功!");
+                    int r = new Random().nextInt(5) + 1;
+                    System.out.println(Thread.currentThread().getName() + " : " + id + " 产假休息需要" + r + "天");
+                    Thread.sleep(r * 1000);
+                    System.out.println(Thread.currentThread().getName() + " : " + id + " 休息产假回来了, 释放许可证...");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    semaphore.release();
+                }
+            });
+        }
+
+        executorService.shutdown();
+    }
+}
