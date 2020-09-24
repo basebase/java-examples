3f1ca1a25da0c8a7a560fbf7130a0fb33753d065
jianfeng.li <jianfeng.li@100credit.com>
2020-09-24 11:45:29 +0800

    数据添加元素方法

--- src/main/java/com/moyu/example/structure/array/Array.java
+++ src/main/java/com/moyu/example/structure/array/Array.java
@@ -27,6 +27,49 @@ public class Array {
         this(10);
     }
 
+
+    /**
+     * 向数组最后一个位置添加元素
+     * @param e   添加元素
+     */
+    public void addLast(int e) {
+        this.add(size, e);
+//        if (size == datas.length)
+//            throw new IllegalArgumentException("addLast添加失败, 请检查数组大小");
+//        this.datas[this.size] = e;
+//        this.size ++; // 添加元素后, 维护size大小, 新添加元素的索引位置
+    }
+
+    public void addFirst(int e) {
+        this.add(0, e);
+    }
+
+    /***
+     * 指定索引位置添加元素
+     * @param index     索引位置
+     * @param e         元素
+     */
+    public void add(int index, int e) {
+        // 1. 判断数组还有没有容量
+        if (size == datas.length)
+            throw new IllegalArgumentException("添加失败, 请检查数组大小");
+
+        // 2. 判断要插入的索引是否合理, 这里我们限制为size, 我们不希望中间出现间隙
+        if (index < 0 || index > size)
+            throw new IllegalArgumentException("请输入正确的索引位置");
+
+        // 将size - 1的元素移动后size位置
+        // 循环结束到要添加指定的位置
+        for (int i = size - 1; i >= index; i--) {
+            this.datas[i + 1] = this.datas[i];
+        }
+
+        // 移动完成后, 覆盖旧值
+        this.datas[index] = e;
+        size ++; // 维护数组元素大小
+    }
+
+
     /**
      * @return    返回数组元素个数
      */
