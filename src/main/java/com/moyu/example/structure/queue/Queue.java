58229ad0736c11b55947e541940a6b2f90195fb3
jianfeng.li <jianfeng.li@100credit.com>
2020-09-29 15:09:36 +0800

    添加队列接口方法注释

--- src/main/java/com/moyu/example/structure/queue/Queue.java
+++ src/main/java/com/moyu/example/structure/queue/Queue.java
@@ -1,9 +1,13 @@
 package com.moyu.example.structure.queue;
 
+/**
+ * 队列接口
+ * @param <E>
+ */
 public interface Queue<E> {
-    void enqueue(E e);
-    E dequeue();
-    E getFront();
+    void enqueue(E e);  // 入队
+    E dequeue();    // 出队
+    E getFront();   // 获取队首元素
     boolean isEmpty();
     int getSize();
 }
